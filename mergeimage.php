bd72f5d14f1d523ace1c55526179768c3be78929
manivannan-manureva <manivannan.g@manurevasolutions.com>
2016-12-02 12:51:38 +0530

    test

--- mergeimage.php
+++ mergeimage.php
@@ -1,6 +1,7 @@
 <?php
 
 function generateFinalImage($config) {
+    //test
     if (isset($config['hd_image_url']) && filter_var($config['hd_image_url'], FILTER_VALIDATE_URL)) {
         $imageData = file_get_contents($config['hd_image_url']);
         //echo '----' . __LINE__ . '----' . __FILE__ . $imageData;
